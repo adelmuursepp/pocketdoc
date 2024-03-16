809b3d68a8fcc75e756260afae1b344d849b8aee
Adel Müürsepp <adel.muursepp@gmail.com>
2024-03-15 23:59:37 -0400

    Put google sheets api call in separate file

--- server/app.py
+++ server/app.py
@@ -2,8 +2,7 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
-from google.oauth2.service_account import Credentials
-from googleapiclient.discovery import build
+from google_sheets import read_google_sheet
 
 load_dotenv()
 
@@ -14,27 +13,10 @@ CONNECTION_STRING = f"mongodb+srv://{os.environ['USERNAME']}:{os.environ['PASSWO
 client = MongoClient(CONNECTION_STRING)
 db = client.your_database_name
 
-# Google sheets API setup
-SERVICE_ACCOUNT_FILE = 'hacktheglobe24-b916c1161289.json'
-SHEET_ID= os.environ['SHEET_ID']
-RANGE_NAME = 'Sheet1!A1:E10'
-
-# Just a helper function to get the data from google sheet
-def read_google_sheet():
-    # Authenticate and construct service
-    creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=['https://www.googleapis.com/auth/spreadsheets'])
-    service = build('sheets', 'v4', credentials=creds)
-
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SHEET_ID, range=RANGE_NAME).execute()
-    values = result.get('values', [])
-    print(values)
-
-    if not values:
-        return 'No data found.'
-    else:
-        return jsonify(values)
+@app.route('/get-calls-summary')
+def get_data_from_google_sheets():
+    data = read_google_sheet()
+    return jsonify(data)
 
 @app.route('/')
 def home():
