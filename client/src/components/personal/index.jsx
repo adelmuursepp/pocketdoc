e827f84f328187e164fff2e46da65224838c6b01
Eugene Zhang <zhang.eug@gmail.com>
2024-03-16 02:36:52 -0400

    Create personal page

--- client/src/components/personal/index.jsx
+++ client/src/components/personal/index.jsx
@@ -1,9 +1,61 @@
+import { useState } from "react";
+
 function Homepage() {
+  const [name, setName] = useState("John Doe");
+  const [age, setAge] = useState(25);
+  const [gender, setGender] = useState("Male");
+  const [symptoms, setSymptoms] = useState("Fever, Cough");
+
   return (
     <div className="max-w-2xl mx-auto">
       <div className="flex flex-col gap-3 mt-[100px] mx-10 items-start">
         <h1 className="font-bold text-2xl">Personal</h1>
-        <div>testing</div>
+        <div className="w-[90%] bg-[#6359BC] p-5 flex flex-col gap-3 rounded">
+          <div className="flex flex-col">
+            <label htmlFor="name" className="text-lg">
+              Name
+            </label>
+            <input
+              className="bg-[#A19BD3] rounded-sm pl-1"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="age" className="text-lg">
+              Age
+            </label>
+            <input
+              className="bg-[#A19BD3] rounded-sm pl-1"
+              id="age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="gender" className="text-lg">
+              Gender
+            </label>
+            <input
+              className="bg-[#A19BD3] rounded-sm pl-1"
+              id="gender"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="symptoms" className="text-lg">
+              Symptoms
+            </label>
+            <input
+              className="bg-[#A19BD3] rounded-sm pl-1"
+              id="symptoms"
+              value={symptoms}
+              onChange={(e) => setSymptoms(e.target.value)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
