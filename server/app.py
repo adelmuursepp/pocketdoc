549740abdd7c9df40c7170007b368f6eb354a438
Eugene Zhang <zhang.eug@gmail.com>
2024-03-16 01:29:35 -0400

    Add backend periodic sheet check

--- server/app.py
+++ server/app.py
@@ -1,27 +1,19 @@
 from dotenv import load_dotenv
-import os
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
-from pymongo import MongoClient
 from google_sheets import read_google_sheet
+from utils.process_data import process_data
+from utils.get_database import get_database
+
 
 load_dotenv()
 
 app = Flask(__name__)
+
 scheduler = BackgroundScheduler(daemon=True)
 
-# MongoDB connection setup
-CONNECTION_STRING = f"mongodb+srv://{os.environ['USERNAME']}:{os.environ['PASSWORD']}@pocketdoccluster.dygbqiw.mongodb.net/pocketdocdb?retryWrites=true&w=majority&appName=pocketdoccluster"
-client = MongoClient(CONNECTION_STRING)
-db = client.your_database_name
-
-def check_google_sheets():
-    # Function to fetch and process data from Google Sheets
-    data = read_google_sheet()
-    print(data)  # Example action: print the data. Replace this with your actual processing logic.
-
 # Schedule the `check_google_sheets` function to run periodically
-scheduler.add_job(check_google_sheets, 'interval', minutes=1) 
+scheduler.add_job(process_data, 'interval', minutes=1) 
 
 @app.route('/get-calls-summary')
 def get_data_from_google_sheets():
@@ -33,6 +25,7 @@ def home():
     return "Hello, Flask with MongoDB!"
 
 if __name__ == '__main__':
+    dbname = get_database()
     scheduler.start()  # Start the scheduler
     try:
         app.run(use_reloader=False)  # Prevents the scheduler from being started twice in debug mode
