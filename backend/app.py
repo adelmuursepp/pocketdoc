a1650a72cd3f8bdba4ef4aeecf04b22b58228ef0
Adel Müürsepp <adel.muursepp@gmail.com>
2024-03-15 23:46:19 -0400

    Organized flask backend

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
 
 load_dotenv()
 
@@ -12,9 +14,32 @@ CONNECTION_STRING = f"mongodb+srv://{os.environ['USERNAME']}:{os.environ['PASSWO
 client = MongoClient(CONNECTION_STRING)
 db = client.your_database_name
 
+# Google sheets API setup
+SERVICE_ACCOUNT_FILE = 'hacktheglobe24-b916c1161289.json'
+SHEET_ID= os.environ['SHEET_ID']
+RANGE_NAME = 'Sheet1!A1:E10'
+
+# Just a helper function to get the data from google sheet
+def read_google_sheet():
+    # Authenticate and construct service
+    creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=['https://www.googleapis.com/auth/spreadsheets'])
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SHEET_ID, range=RANGE_NAME).execute()
+    values = result.get('values', [])
+    print(values)
+
+    if not values:
+        return 'No data found.'
+    else:
+        return jsonify(values)
+
 @app.route('/')
 def home():
     return "Hello, Flask with MongoDB!"
 
 if __name__ == '__main__':
+    read_google_sheet()
     app.run(debug=True)
