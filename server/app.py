a347db2f21a2056b4ffdfdc41329c7dc3d49a5e5
Adel Müürsepp <adel.muursepp@gmail.com>
2024-03-16 01:33:09 -0400

    Added mistral api calls

--- server/app.py
+++ server/app.py
@@ -4,6 +4,11 @@ from flask import Flask, jsonify, request
 from apscheduler.schedulers.background import BackgroundScheduler
 from pymongo import MongoClient
 from google_sheets import read_google_sheet
+from openai import OpenAI
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+
+
 
 load_dotenv()
 
@@ -14,6 +19,16 @@ scheduler = BackgroundScheduler(daemon=True)
 CONNECTION_STRING = f"mongodb+srv://{os.environ['USERNAME']}:{os.environ['PASSWORD']}@pocketdoccluster.dygbqiw.mongodb.net/pocketdocdb?retryWrites=true&w=majority&appName=pocketdoccluster"
 client = MongoClient(CONNECTION_STRING)
 db = client.your_database_name
+
+# OpenAI API call setup
+# openai.api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI()
+
+# Mistral API call setup
+
+mistral_api_key = os.environ["MISTRAL_API_KEY"]
+model = "mistral-tiny"
+client = MistralClient(api_key=mistral_api_key)
 
 def check_google_sheets():
     # Function to fetch and process data from Google Sheets
@@ -31,6 +46,50 @@ def get_data_from_google_sheets():
 @app.route('/')
 def home():
     return "Hello, Flask with MongoDB!"
+
+@app.route('/recommendations', methods=['GET'])
+def get_cough_recommendations():
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+    )
+    return response
+
+    # prompt = "Give me recommendations for getting better when I have a cough."
+    
+    # try:
+    #     response = openai.Completion.create(
+    #         engine="gpt-4",  
+    #         prompt=prompt,
+    #         temperature=0.1,  # Adjust for creativity. Lower is more deterministic.
+    #         max_tokens=100,  # Adjust based on how detailed you want the response to be
+    #         top_p=1.0,
+    #         frequency_penalty=0.0,
+    #         presence_penalty=0.0
+    #     )
+    #     recommendations = response.choices[0].text.strip()
+    #     return jsonify({"recommendations": recommendations})
+    # except Exception as e:
+    #     return jsonify({"error": str(e)}), 500
+
+@app.route('/fetch-data')
+def fetch_data():
+    messages = [
+        ChatMessage(role="user", content="I have cough, what should I do?")
+    ]
+
+    chat_response = client.chat(
+        model=model,
+        messages=messages,
+    )
+    message = chat_response.choices[0].message.content
+    return(jsonify(message))
+
 
 if __name__ == '__main__':
     scheduler.start()  # Start the scheduler
