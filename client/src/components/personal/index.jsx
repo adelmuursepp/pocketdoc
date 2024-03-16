4fa1c873be36631a6df24be8b6ad4f696f4294dc
Eugene Zhang <zhang.eug@gmail.com>
2024-03-15 23:46:58 -0400

    Setup routes + homepage

---
+++ client/src/components/personal/index.jsx
@@ -0,0 +1,12 @@
+function Homepage() {
+  return (
+    <div className="max-w-2xl mx-auto">
+      <div className="flex flex-col gap-3 mt-[100px] mx-10 items-start">
+        <h1 className="font-bold text-2xl">Personal</h1>
+        <div>testing</div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from "./components/homepage/index.jsx";
+import Personal from "./components/personal/index.jsx";
+import Dashboard from "./components/dashboard/index.jsx";
+import pocketDocLogo from "./assets/pocketdoc.png";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <nav>
+          <ul className="flex items-center justify-around">
+            <li>
+              <Link to="/">
+                <img src={pocketDocLogo} className="h-32" />
+              </Link>
+            </li>
+            <div className="flex gap-4 text-sm">
+              <li>
+                <Link to="personal">Personal</Link>
+              </li>
+              <li>
+                <Link to="dashboard">Dashboard</Link>
+              </li>
+            </div>
+          </ul>
+        </nav>
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "personal",
+        element: <Personal />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
